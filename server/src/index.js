bd3bfb24f3a7e37e2d2af3ed3299aa4acb072683
jaylinrose <jaylin.manuel02@gmail.com>
2023-08-09 21:17:31 -0700

    created routes directory, imported bcyrpt, express, and jsonwebtoken in users.js

--- server/src/index.js
+++ server/src/index.js
@@ -6,10 +6,14 @@ import mongoose from 'mongoose'
 
 const app = express()
 
+import { userRouter } from "./routes/users.js";
+
 //Middleware
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect("mongodb+srv://jaylinmanuel02:FF4Utbkr3K27Ninv@recipes.yn0brrg.mongodb.net/recipes?retryWrites=true&w=majority");
 

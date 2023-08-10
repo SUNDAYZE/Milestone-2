6bda4f4ec25a964abe4f8adae0ea6a1330a6c6f0
Heriberto Delgado Jr <heribertodelgado92@gmail.com>
2023-08-09 19:23:55 -0700

    Setting up MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
-import express from 'express'; 
+import express from 'express';
 import cors from 'cors';
- import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
- const app = express();
+const app = express();
 
- app.use(express.json());
- app.use(cors());
+app.use(express.json());
+app.use(cors());
 
- app.listen(3001, () => console.log("SERVER STARTED"));
+mongoose.connect(
+    "mongodb+srv://milestone2:Assessors1316@recipes.nziaaqn.mongodb.net/");
+
+app.listen(3001, () => console.log("SERVER STARTED"));

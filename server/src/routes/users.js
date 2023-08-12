de598127e6a0c0b2132f31e4683f2cfc725c35ba
Heriberto Delgado Jr <heribertodelgado92@gmail.com>
2023-08-12 11:37:04 -0700

    fixing login path bugs

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -10,23 +10,32 @@ router.post("/register", async (req, res) => {
     console.log(req.body)
     const user = await UserModel.findOne({ username });
 
-if (user) {
-    return res.json({message: "User already exists!"});
-}
-     const hashedPassword = await bcrypt.hash(password, 10)
-     const newUser = new UserModel({username, password: hashedPassword});
-     await newUser.save();
+    if (user) {
+        return res.json({ message: "User already exists!" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
 
-    res.json({message: "User Registered Successfully"});
+    res.json({ message: "User Registered Successfully" });
 });
 
-router.post("/login", async (res,req) => {
-    const {username, password} =req.body;
-    const user = await UserModel.findOne({username});
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
 
-    if (!user){
-        return res.json({ message: "User Does Not Exist!"});
+    if (!user) {
+        return res.json({ message: "User Does Not Exist!" });
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.json({ message: "Username or Password Is Incorrect" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, userID: user._id });
 });
 
 export { router as userRouter }; 

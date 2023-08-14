db376d5e574e39df9b3e176b818c23aa8bc80e8a
jscf222 <jscf222@gmail.com>
2023-08-14 14:27:09 -0700

    user login and password

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -7,20 +7,38 @@ import { UserModel } from '../models/Users.js'
 
 const router = express.Router()
 
-router.post('/register', async (req, res) =>{
+router.post('/register', async (req, res) => {
     const { username, password } = req.body
 
-    const user = await UserModel.findOne({ username})
-    
-    if (user) {
-        return res.json({message:'User already exists!'} )
-    }
-const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await UserModel.findOne({ username })
 
-const newUser = new UserModel ({ username, password: hashedPassword})
-await newUser.save()
-res.json({message:"User Registered Successfully"})
+    if (user) {
+        return res.json({ message: 'User already exists!' })
+    }
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    const newUser = new UserModel({ username, password: hashedPassword })
+    await newUser.save()
+    res.json({ message: "User Registered Successfully" })
 })
 
-router.post('/login')
-export {router as userRouter}
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    const user = await UserModel.findOne({ username })
+    if (!user) {
+        return res.json({ message: "User Doens't Exists!!" })
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+        return res.json({ message: 'Username or Password Is Incorrect!!' })
+
+    }
+    const token = jwt.sign({ id: user._id }, 'secret')
+    res.json({token, userID: user._id})
+
+
+
+})
+
+export { router as userRouter }

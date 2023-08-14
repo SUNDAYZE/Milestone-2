43f7e0630600b41830119b555d9bfcfe8f23e31f
jscf222 <jscf222@gmail.com>
2023-08-14 13:50:58 -0700

    adding back end

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import {userRouter} from './routes/users.js'
 
 const app = express()
 
@@ -9,5 +10,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use('/auth', userRouter)
+
+mongoose.connect("mongodb+srv://jscf222:MBBLecq0Hlae@recipe.uqprtdw.mongodb.net/recipe")
 
 app.listen(3001, () => console.log('SERVER STARTED!'))

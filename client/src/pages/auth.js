90376fb7ce39d27c5f716f21adfb53cfcab5bd97
Isaac Rodriguez Vega <jscf222@gmail.com>
2023-08-15 12:10:15 -0500

    creating register ui

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,3 +1,36 @@
+import {useState} from 'react'
+
+
 export const Auth = () => {
-    return <div>Auth </div>
+    return <div className='auth'>
+        <Login/>
+        <Register/>
+         
+        </div>
+}
+
+const Login = () => {
+    return <div> </div>
+
+}
+
+const Register = () => {
+const [username, setUsername] = useState('')
+const [password, setPassword] = useState('')
+
+    
+    return <div className='auth-container'>
+        <form>
+            <h2> Register</h2>
+            <div className='form-group'>
+                <label htmlFor='username'>Username:</label>
+                <input type='text' id='usename' value ={username} onChange={(event) => setUsername(event.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label htmlFor='password'>Password:</label>
+                <input type='text' id='password' value={password} onChange={(event) =>  setPassword(event.target.value)}/>
+            </div>
+
+        </form>
+    </div>
 }

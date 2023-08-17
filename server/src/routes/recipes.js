f66a4ebf4f74852b99d17d2c54452014e07476b1
jaylinrose <jaylin.manuel02@gmail.com>
2023-08-17 11:03:11 -0700

    updated last of routes

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose"
 import { RecipeModel } from "../models/Recipes.js";
 import { UserModel } from "../models/Users.js";
+import { verifyToken } from "./users.js";
 
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.get("/", async (req, res) => {
 
 });
 
-router.post("/", async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
     const recipe = new RecipeModel(req.body)
     try{
         const response = await recipe.save();
@@ -26,7 +27,7 @@ router.post("/", async (req, res) => {
 
 })
 
-router.put("/", async (req, res) => {
+router.put("/", verifyToken, async (req, res) => {
     try{
         const recipe = await RecipeModel.findById(req.body.recipeID)
         const user = await UserModel.findById(req.body.userID)
@@ -49,9 +50,9 @@ router.get("/savedRecipes/ids/:userID", async (req, res) => {
 
 })
 
-router.get("/savedRecipes", async (req, res) => {
+router.get("/savedRecipes/:userID", async (req, res) => {
     try{
-        const user = await UserModel.findById(req.body.userID);
+        const user = await UserModel.findById(req.params.userID);
         const savedRecipes = await RecipeModel.find ({
             _id: { $in: user.savedRecipes },
         });
@@ -63,4 +64,6 @@ router.get("/savedRecipes", async (req, res) => {
 })
 
 export {router as recipesRouter}
+
+
 
